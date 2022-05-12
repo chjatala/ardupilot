b3d31c25d79668f08d5f452826d0f4ac43a178b2
Hafeez M Chaudhary <mh.chaudhary@yahoo.com>
2022-05-12 14:20:41 +0200

    added agilica beacon mode

---
+++ libraries/AP_Beacon/AP_Beacon_Agilica.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "AP_Beacon_Backend.h"
+
+#define AGILICA_MSG_BUF_MAX      (255)
+
+class AP_Beacon_Agilica : public AP_Beacon_Backend
+{
+
+public:
+    // constructor
+    AP_Beacon_Agilica(AP_Beacon &frontend, AP_SerialManager &serial_manager);
+
+    // return true if sensor is basically healthy (we are receiving data)
+    bool healthy() override;
+
+    // update the state of the sensor
+    void update() override;
+
+private:
+    // parse vehicle pos message 
+    void parse_vehicle_pos_msg(const uint32_t num_beacon);
+    // parse beacon pos message 
+    void parse_beacon_pos_msg(const uint32_t num_beacon);
+    //verify message checksum
+    bool verify_checksum();
+
+    AP_HAL::UARTDriver *_uart = nullptr;
+
+    enum class ParseState : uint8_t {
+        Waiting_Sync = 0,       // waiting for msg sync
+        Waiting_Len = 1,        // waiting for message len
+        Waiting_Payload = 2     // waiting for message payload
+    };
+    
+    ParseState _parse_state = ParseState::Waiting_Sync;
+
+    // members
+    uint8_t _msg_buf[AGILICA_MSG_BUF_MAX];      // buffer to hold most recent message from tag
+    uint8_t  _write_index_buf = 0;              // write index for the msg_buf
+    uint16_t _msg_len;                          // received message length    
+    uint32_t _last_update_ms;                   // last time we receive data from tag
+};
