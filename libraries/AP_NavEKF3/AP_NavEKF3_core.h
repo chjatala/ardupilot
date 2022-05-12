bcb4bcb834d9dd1826dff87929a6bd1b3ce150b5
Hafeez M Chaudhary <mh.chaudhary@yahoo.com>
2022-05-12 14:14:48 +0200

    added agilica beacon mode

--- libraries/AP_NavEKF3/AP_NavEKF3_core.h
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.h
@@ -1249,7 +1249,12 @@ private:
     bool rngBcnHealth;                  // boolean true if range beacon measurements have passed innovation consistency check
     ftype varInnovRngBcn;               // range beacon observation innovation variance (m^2)
     ftype innovRngBcn;                  // range beacon observation innovation (m)
-    uint32_t lastTimeRngBcn_ms[4];      // last time we received a range beacon measurement (msec)
+    //4 was the original size, but CMH changed to AP_BEACON_MAX_BEACONS to avoid assertion error in ardupilot/libraries/AP_NavEKF3/AP_NavEKF3_Measurements.cpp
+#if (AP_BEACON_MAX_BEACONS > 4)
+    uint32_t lastTimeRngBcn_ms[AP_BEACON_MAX_BEACONS]; // last time we received a range beacon measurement (msec) 
+#else
+    uint32_t lastTimeRngBcn_ms[4]; // last time we received a range beacon measurement (msec) 
+#endif
     bool rngBcnDataToFuse;              // true when there is new range beacon data to fuse
     Vector3F beaconVehiclePosNED;       // NED position estimate from the beacon system (NED)
     ftype beaconVehiclePosErr;          // estimated position error from the beacon system (m)
